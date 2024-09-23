0e439f236da779d33808aea00fd5f92bbf7fa30e
Yury Sulsky <yury.sulsky@gmail.com>
2024-09-22 18:52:02 -0700

    custom vars

--- .emacs.d/custom-vars.el
+++ .emacs.d/custom-vars.el
@@ -7,6 +7,8 @@
     ["#242730" "#ff665c" "#7bc275" "#FCCE7B" "#51afef" "#C57BDB" "#5cEfFF" "#bbc2cf"])
  '(custom-safe-themes
     '("7e377879cbd60c66b88e51fad480b3ab18d60847f31c435f15f5df18bdb18184" "013728cb445c73763d13e39c0e3fd52c06eefe3fbd173a766bfd29c6d040f100" "1f292969fc19ba45fbc6542ed54e58ab5ad3dbe41b70d8cb2d1f85c22d07e518" "81f53ee9ddd3f8559f94c127c9327d578e264c574cda7c6d9daddaec226f87bb" "ffafb0e9f63935183713b204c11d22225008559fa62133a69848835f4f4a758c" "9f297216c88ca3f47e5f10f8bd884ab24ac5bc9d884f0f23589b0a46a608fe14" "88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e" "d6b934330450d9de1112cbb7617eaf929244d192c4ffb1b9e6b63ad574784aad" default))
+ '(display-time-default-load-average nil)
+ '(display-time-mode t)
  '(exwm-floating-border-color "#1c1f24")
  '(fci-rule-color "#62686E")
  '(highlight-tail-colors
@@ -20,7 +22,7 @@
  '(jdee-db-spec-breakpoint-face-colors (cons "#1c1f24" "#484854"))
  '(objed-cursor-color "#ff665c")
  '(package-selected-packages
-    '(projectile shfmt treesit-auto vertico orderless which-key magit company rainbow-delimiters fzf yaml-mode))
+    '(doom-modeline bash-lsp lsp-shell lsp-bash ripgrep projectile shfmt treesit-auto vertico orderless which-key magit company rainbow-delimiters fzf yaml-mode))
  '(pdf-view-midnight-colors (cons "#bbc2cf" "#242730"))
  '(rustic-ansi-faces
     ["#242730" "#ff665c" "#7bc275" "#FCCE7B" "#51afef" "#C57BDB" "#5cEfFF" "#bbc2cf"])
@@ -56,4 +58,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :extend nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "xos4" :family "terminus"))))
+ '(doom-modeline-time ((t (:foreground "brightcyan" :inherit doom-modeline))))
  '(region ((t (:extend t :background "#505050")))))
